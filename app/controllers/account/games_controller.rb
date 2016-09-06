7281841c951b812e4b57e567f3df5d28e5a11e6b
Lars Bohm <larsbohm@gmail.com>
2016-09-06 22:16:48 +0200

    add initial routes and controllers

---
+++ app/controllers/account/games_controller.rb
@@ -0,0 +1,19 @@
+class Account::GamesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,24 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :account do
+    resources :games, except: [:show]
+  end
+
+  resources :games, only: :show do 
+    resources :playing_sessions do
+      resources :rpg_characters
+      resources :chapters do
+        resources :encounters do
+          resources :lores
+          resources :action_events do
+            resources :monsters
+            resources :initiatives
+            resources :actions
+          end
+        end
+      end
+    end
+  end
 end

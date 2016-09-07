e377d8bed7fa4d78ce1eaa823cadf6dedc157930
Lars Bohm <larsbohm@gmail.com>
2016-09-07 20:20:50 +0200

    setup configuration for redis on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.web_socket_server_url = "wss://tabletopr.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://tabletopr.herokuapp.com', 'http://tabletopr.herokuapp.com']
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
